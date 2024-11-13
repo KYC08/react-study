705f02394a38f3d96e18ad0aa61758f5db8b146b
youngchan <yckim8721@gmail.com>
2024-11-13 16:33:59 +0900

    Notice, Notice_modal(recoil, axios, portal)_241113

---
+++ src/component/common/portal/Portal.tsx
@@ -0,0 +1,10 @@
+import { FC, ReactNode } from "react";
+import ReactDOM from "react-dom";
+
+interface PortalProps {
+  children: ReactNode;
+}
+
+export const Portal: FC<PortalProps> = ({ children }) => {
+  return ReactDOM.createPortal(children, document.body);
+};
--- src/component/page/Notice/NoticeModal/NoticeModal.tsx
+++ src/component/page/Notice/NoticeModal/NoticeModal.tsx
@@ -1,31 +1,40 @@
-import { NoticeModalStyled } from './styled';
+import { FC } from "react";
+import { NoticeModalStyled } from "./styled";
+import { useRecoilState } from "recoil";
+import { modalState } from "../../../../stores/modalState";
 
 export const NoticeModal = () => {
-    return (
-        <NoticeModalStyled>
-            <div className="container">
-                <label>
-                    제목 :<input type="text"></input>
-                </label>
-                <label>
-                    내용 : <input type="text"></input>
-                </label>
-                파일 :<input type="file" id="fileInput" style={{ display: 'none' }}></input>
-                <label className="img-label" htmlFor="fileInput">
-                    파일 첨부하기
-                </label>
-                <div>
-                    <div>
-                        <label>미리보기</label>
-                        <img src="" />
-                    </div>
-                </div>
-                <div className={'button-container'}>
-                    <button>저장</button>
-                    <button>삭제</button>
-                    <button>나가기</button>
-                </div>
-            </div>
-        </NoticeModalStyled>
-    );
+  const [modal, setModal] = useRecoilState<boolean>(modalState);
+
+  const handlerModal = () => {
+    setModal(!modal);
+  };
+  return (
+    <NoticeModalStyled>
+      <div className="container">
+        <label>
+          제목 :<input type="text"></input>
+        </label>
+        <label>
+          내용 : <input type="text"></input>
+        </label>
+        파일 :
+        <input type="file" id="fileInput" style={{ display: "none" }}></input>
+        <label className="img-label" htmlFor="fileInput">
+          파일 첨부하기
+        </label>
+        <div>
+          <div>
+            <label>미리보기</label>
+            <img src="" />
+          </div>
+        </div>
+        <div className={"button-container"}>
+          <button>저장</button>
+          <button>삭제</button>
+          <button onClick={handlerModal}>나가기</button>
+        </div>
+      </div>
+    </NoticeModalStyled>
+  );
 };
